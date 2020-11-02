38cb6f065cd4104e15cca170ae9afced383c91ec
hexchen <hexchen@lilwit.ch>
2020-11-02 19:24:40 +0000

    linuxPackages.decklink: init at 12.0a14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20752,6 +20752,8 @@ in
 
     ddcci-driver = callPackage ../os-specific/linux/ddcci { };
 
+    decklink = callPackage ../os-specific/linux/decklink { };
+
     digimend = callPackage ../os-specific/linux/digimend { };
 
     dpdk-kmods = callPackage ../os-specific/linux/dpdk-kmods { };
