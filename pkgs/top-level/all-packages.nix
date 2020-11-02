0d4da173296773153a009ca51139ab0d9e2b0b5f
hexchen <hexchen@lilwit.ch>
2020-11-02 19:24:40 +0000

    linuxPackages.decklink: init at 1.16
    Also add myself to maintainers list

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17791,6 +17791,8 @@ in
 
     ddcci-driver = callPackage ../os-specific/linux/ddcci { };
 
+    decklink = callPackage ../os-specific/linux/decklink { };
+
     digimend = callPackage ../os-specific/linux/digimend { };
 
     dpdk = callPackage ../os-specific/linux/dpdk { };
