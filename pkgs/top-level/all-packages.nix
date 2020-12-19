cfb0acca68d35154de9502b130343fc1998f4200
hexchen <hexchen@lilwit.ch>
2020-12-19 20:52:28 +0000

    voctogui: init at 2.0a

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23810,6 +23810,8 @@ in
 
   vocal = callPackage ../applications/audio/vocal { };
 
+  voctogui = callPackage ../applications/video/voctogui { };
+
   vogl = libsForQt5.callPackage ../development/tools/vogl { };
 
   volnoti = callPackage ../applications/misc/volnoti { };
