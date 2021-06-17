c103e8be27da4e73e89a5cad49ec23755a11c05e
hexchen <hexchen@lilwit.ch>
2021-06-17 01:20:35 +0000

    maintainers: add hexchen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4097,6 +4097,12 @@
     githubId = 131599;
     name = "Martin Weinelt";
   };
+  hexchen = {
+    email = "nix@lilwit.ch";
+    github = "hexchen";
+    githubId = 41522204;
+    name = "hexchen";
+  };
   hh = {
     email = "hh@m-labs.hk";
     github = "HarryMakes";
